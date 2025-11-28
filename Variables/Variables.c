30d58cb7ea2b469e033053c17ec1c8c2c99216e9
lefirayen <230567556+lefirayen@users.noreply.github.com>
2025-11-28 19:46:07 +0100

    Initialize variables and print their values
    Add variable declarations and print statements

---
+++ Variables/Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int quantity = 5;
+    float price = 5.99;
+    double length = 5.43342;
+    char Letter = 'A';
+
+    printf("Quantity: %d");
+    printf("Price: %f");
+    printf("Length in CM: %lf");
+    printf("Letter: %c");
+
+    return 0;
+}
