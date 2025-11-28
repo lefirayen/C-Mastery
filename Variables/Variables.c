648ba158009db2f6b09a9c33f967283e9102922d
lefirayen <230567556+lefirayen@users.noreply.github.com>
2025-11-28 19:48:54 +0100

    Update Variables.c

--- Variables/Variables.c
+++ Variables/Variables.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-    int quantity = 5;
-    float price = 5.99;
-    double length = 5.43342;
-    char Letter = 'A';
+    int quantity = 5; // Integer variable: stores whole numbers (no decimals)
+    float price = 5.99; // Float variable: stores decimal numbers (lower precision)
+    double length = 5.43342; // Double variable: stores decimal numbers (higher precision)
+    char Letter = 'A'; // Char variable: stores a single character
 
     printf("Quantity: %d");
     printf("Price: %f");
@@ -14,3 +14,4 @@ int main(void)
 
     return 0;
 }
+
